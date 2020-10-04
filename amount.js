4960ec9446dadb29f242fadf614fd3fa7d0634eb
nasir <nasir.uddin@bs-23.net>
2020-10-04 20:44:17 +0600

    add event listener bug fix

--- amount.js
+++ amount.js
@@ -4,16 +4,26 @@
 	*** Finance js lib development
 	*** Author/Developer: Md. Nasir Uddin Bhuiyan
 */
-var __splitInputValueOnKeyPress = 0;
-	
-	function validAmountOnKeyUp (element) {
-		if(!isValidInputValue(element.value)){
-			element.value = '';
+	var __splitInputValueOnKeyPress = 0;
+	/*document.addEventListener('DOMContentLoaded',function(){
+		var amountInputs = document.getElementsByClassName("amount");
+		for(var i=0; i<amountInputs.length; i++){
+					amountInputs[i].onkeyup = validAmount(amountInputs[i]);
+					amountInputs[i].onkeypress = validChar(amountInputs[i]);
+					amountInputs[i].onpaste = pasteFilter(amountInputs[i]);
+		}
+	});*/
+
+	function validAmount () {
+		console.log("validAmount"+this.value);
+		if(!isValidInputValue(this.value)){
+			this.value = '';
 			return;
 		}
 
-		var cursorPosition = element.selectionStart;
-		var elValue = element.value.split(".");
+		var cursorPosition = this.selectionStart;
+		console.log("cursorPosition" + cursorPosition);
+		var elValue = this.value.split(".");
 		var value = elValue[0].replace(/,/g, '');
 		var decimalPart = elValue.length > 1? '.' + elValue[1] : '';
 
@@ -47,29 +57,29 @@ var __splitInputValueOnKeyPress = 0;
 		}
 
 		result = result + (decimalPart.length <= 3 ? decimalPart : decimalPart.substring(0,3));
-		element.value = result;
+		this.value = result;
 
-		if(!isMobileOrTablet()) {
-			var charCode = (element.which) ? element.which : event.keyCode;
-			var cursorPositionAdder = 0;
-			if((charCode >=48 && charCode <=57) || (charCode >=96 && charCode <=105) || charCode == 188 || charCode == 190){
-				cursorPositionAdder = result.substring(0,cursorPosition).split(",").length - __splitInputValueOnKeyPress;
-			}
-			console.log("cursorPositionAdder" + cursorPositionAdder);
-			element.selectionStart = cursorPosition + (cursorPositionAdder>0? cursorPositionAdder : 0);
-			element.selectionEnd = cursorPosition + (cursorPositionAdder>0? cursorPositionAdder : 0);
+		var charCode = (this.which) ? this.which : this.keyCode;
+		var cursorPositionAdder = 0;
+		console.log("__splitInputValueOnKeyPress" + __splitInputValueOnKeyPress);
+		if((charCode >=48 && charCode <=57) || (charCode >=96 && charCode <=105) || charCode == 188 || charCode == 190){
+			cursorPositionAdder = result.substring(0,cursorPosition).split(",").length - __splitInputValueOnKeyPress;
 		}
+		console.log("cursorPositionAdder" + cursorPositionAdder);
+		this.selectionStart = cursorPosition + (cursorPositionAdder>0? cursorPositionAdder : 0);
+		this.selectionEnd = cursorPosition + (cursorPositionAdder>0? cursorPositionAdder : 0);
 	}
 
-	function validAmountOnKeyPress (element) {
+	function validChar (event) {
+		console.log("validChar"+event.value);
 		try {
-			if(element.selectionStart !=0) {
-				__splitInputValueOnKeyPress = element.value.substring(0,element.selectionStart).split(",").length;
+			if(event.selectionStart !=0) {
+				__splitInputValueOnKeyPress = event.value.substring(0,event.selectionStart).split(",").length;
 			}
 		} catch(e){
 			__splitInputValueOnKeyPress = 0;
 		}
-		var charCode = (element.which) ? element.which : event.keyCode;
+		var charCode = (event.which) ? event.which : event.keyCode;
 		if(!isValidInputValue(String.fromCharCode(charCode))){
 			return false;
 		}
@@ -83,18 +93,26 @@ var __splitInputValueOnKeyPress = 0;
 		return /^-?[\d,.]*$/.test(value);
 	}
 
-	function isMobileOrTablet() {
-		const toMatch = [
-	        /Android/i,
-	        /webOS/i,
-	        /iPhone/i,
-	        /iPad/i,
-	        /iPod/i,
-	        /BlackBerry/i,
-	        /Windows Phone/i
-	    ];
-
-	    return toMatch.some((toMatchItem) => {
-	        return navigator.userAgent.match(toMatchItem);
-	    });
+	function pasteFilter(elem){
+		console.log("pasteFilter"+elem.value);
+		if(elem.value != null && elem.value != '' && elem.value.length > 0) {
+			var intiResult = '';
+			var valueArray = elem.value.split('');
+			for(var i=0; i<valueArray.length; i++){
+				if(/^-?[\d]+$/.test(valueArray[i])){
+					intiResult+=valueArray[i];
+				}
+			}
+			elem.value=intiResult;
+		}
+		return true;
 	}
+
+	(function(){
+		var amountInputs = document.getElementsByClassName("amount");
+		for(var i=0; i<amountInputs.length; i++){
+			amountInputs[i].addEventListener("keyup", validAmount);
+			amountInputs[i].addEventListener("keypress", (event) => {event.returnValue =  validChar(event)});
+			// amountInputs[i].onpaste = function() {pasteFilter()};
+		}
+	})();
